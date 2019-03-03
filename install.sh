6b308d307d0d231d625a9b7287cd506c58d619f0
Ethan Houston <ethan.houston@gmail.com>
2019-03-03 11:19:43 -0600

    Update .vimrc

--- install.sh
+++ install.sh
@@ -3,11 +3,16 @@
 clear
 
 echo "Copying vimrc"
+echo ".vimrc -> ~/.vimrc"
 cp ~/.vimrc .vimrc
+echo "Done copying"
 
 echo "Uploading to GitHub"
+echo "Adding files"
 git add .
+echo "Committing files"
 git commit -m "Update .vimrc"
+echo "Pushing files"
 git push
 
 echo "Done!"

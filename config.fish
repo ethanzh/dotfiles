0171aabd919e0f47a576a42f251190e92cf64102
Ethan Houston <ethan.houston@gmail.com>
2019-10-10 09:27:57 -0500

    Add venv shortcut

--- config.fish
+++ config.fish
@@ -3,5 +3,7 @@ set -gx PATH ~/anaconda3/bin $PATH
 abbr -a -g fishc vim ~/.config/fish/config.fish
 abbr -a -g gs "git status"
 
+abbr -a -g activate ". venv/bin/activate.fish"
+
 abbr -a -g dots "cd ~/Code/dotfiles;rsync config.fish ~/.config/fish/config.fish; rsync .vimrc ~/.vimrc"
 

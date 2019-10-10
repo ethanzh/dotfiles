3ab65f97c78041255ef05f17e446c170135e4e4f
Ethan Houston <ethan.houston@gmail.com>
2019-10-10 10:01:19 -0500

    Add returns on cd

--- config.fish
+++ config.fish
@@ -7,8 +7,10 @@ abbr -a -g gs "git status"
 abbr -a -g activate ". venv/bin/activate.fish"
 
 abbr -a -g dots "cd ~/Code/dotfiles"
-abbr -a -g udots "cd ~/Code/dotfiles;git pull;rsync config.fish ~/.config/fish/config.fish; rsync .vimrc ~/.vimrc; source ~/.config/fish/config.fish"
+abbr -a -g udots "cd ~/Code/dotfiles;git pull;rsync config.fish ~/.config/fish/config.fish; rsync .vimrc ~/.vimrc; source ~/.config/fish/config.fish; cd -"
 
 abbr -a -g status "echo $status"
 
 abbr -a -g lab "ssh ethanzh@kierkegaard.cs.utexas.edu"
+
+abbr -a -g resume "cd ~/Code/latex-resume;echo houston_ethan_resume.tex | xelatex; cd -"

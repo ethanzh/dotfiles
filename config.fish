964180548efea5cd0fe870e0e99fc5120947b103
Ethan Houston <ethan.houston@gmail.com>
2019-10-10 09:42:45 -0500

    Various fixes

--- config.fish
+++ config.fish
@@ -1,9 +1,11 @@
 set -gx PATH ~/anaconda3/bin $PATH
+set fish_greeting
 
 abbr -a -g fishc vim ~/.config/fish/config.fish
 abbr -a -g gs "git status"
 
 abbr -a -g activate ". venv/bin/activate.fish"
 
-abbr -a -g dots "cd ~/Code/dotfiles;rsync config.fish ~/.config/fish/config.fish; rsync .vimrc ~/.vimrc"
+abbr -a -g dots "cd ~/Code/dotfiles"
+abbr -a -g udots "cd ~/Code/dotfiles;git pull;rsync config.fish ~/.config/fish/config.fish; rsync .vimrc ~/.vimrc; source ~/.config/fish/config.fish"
 

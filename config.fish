5914d20e32078e4b9547d1a21cf640e930111913
Ethan Houston <ethan.houston@gmail.com>
2020-02-11 12:15:53 -0600

    Add course aliases

--- config.fish
+++ config.fish
@@ -1,4 +1,5 @@
 set -gx PATH ~/anaconda3/bin $PATH
+set -gx PATH /Users/ethanzh/.local/bin $PATH
 set fish_greeting
 
 abbr -a -g fishc vim ~/.config/fish/config.fish
@@ -14,3 +15,9 @@ abbr -a -g status "echo $status"
 abbr -a -g lab "ssh ethanzh@kierkegaard.cs.utexas.edu"
 
 abbr -a -g resume "cd ~/Code/latex-resume;echo houston_ethan_resume.tex | xelatex; cd -"
+
+abbr -a -g todo "cd ~/todo;cat todo;cd --"
+
+abbr -a -g pl "cd ~/UT/Courses/CS345"
+abbr -a -g db "cd ~/UT/Courses/CS327E"
+abbr -a -g intro "cd ~/UT/Courses/CS361"

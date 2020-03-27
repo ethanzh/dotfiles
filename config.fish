e94c3e87a98077573b00b32e88bb9264876aec7a
Ethan Houston <ethan.houston@gmail.com>
2020-03-26 18:04:56 -0700

    Edit config.fish

--- config.fish
+++ config.fish
@@ -15,6 +15,9 @@ abbr -a -g activate ". venv/bin/activate.fish"
 # utcs lab
 abbr -a -g lab "ssh ethanzh@kierkegaard.cs.utexas.edu"
 
+# feverbase server
+abbr -a -g fever "ssh root@159.65.71.64"
+
 switch (uname)
 	case Linux
 		set -gx PATH /home/linuxbrew/.linuxbrew/bin $PATH
@@ -25,6 +28,12 @@ abbr -a -g dots "cd ~/Code/dotfiles/"
 
 # shortcut to recorda
 abbr -a -g recorda "cd ~/Code/recorda-django/recorda/api; vim -p views.py models.py urls.py"
+
+# tidy
+switch (uname)
+    case Darwin
+        abbr -a -g tidy "/usr/local/Cellar/tidy-html5/5.6.0/bin/tidy"
+end
 
 # shortcuts to courses
 switch (uname)
@@ -54,6 +63,9 @@ abbr -a -g pipr "activate;pip install -r requirements.txt"
 # git shortcuts
 abbr -a -g gs "git status"
 abbr -a -g glog "git log --all --decorate --oneline --graph"
+
+# ls shortcut
+abbr -a -g l "ls -la"
 
 # thefuck plugin
 thefuck --alias | source
